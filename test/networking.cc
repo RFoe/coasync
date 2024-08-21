72c7b37edadfbdd257cd654b0704c6ff8d3fa8a5
RFoer <3069656029@qq.com>
2024-08-21 22:47:10 +0800

    .

--- test/networking.cc
+++
@@ -1,9 +0,0 @@
-#include "../include/coasync/detail/networking.hpp"
-using namespace coasync;
-int main() {
-	WSADATA wsa_data;
-	::WSAStartup(MAKEWORD(2, 2), std::addressof(wsa_data));
-	std::printf("%s\n", ::gai_strerror(EAI_NONAME));
-	throw std::system_error(EAI_NONAME, detail::generic_category());
- 	WSACleanup();
-}
